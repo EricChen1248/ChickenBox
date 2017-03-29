74f126ed07ad9d95ab9604c0fed400fa4e64a329
EricChen1248 <ericchen1248@gmail.com>
2017-03-29 16:52:50 +0800

    Startu

---
+++ ChickenBox/Building_ChickenNest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using RimWorld.Planet;
+using Verse;
+using Verse.AI;
+
+namespace ChickenBox
+{
+    class Building_ChickenNest : Building_Bed
+    {
+         
+    }
+}
