a8258518a2c995d86f66cafbeb9c23589bd40e79
EricChen1248 <ericchen1248@gmail.com>
2017-03-29 16:52:50 +0800

    Basic Setup

---
+++ ChickenBox/JobGiver_FindChickenBox.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace ChickenBox
+{
+    public class JobGiver_FindChickenBox : ThinkNode_JobGiver
+    {
+        private const int MaxSearchDistance = 30;
+        
+        protected override Job TryGiveJob(Pawn pawn)
+        {
+            
+            var comp = pawn.TryGetComp<CompEggLayer>();
+            if (comp == null || !comp.CanLayNow)
+                return null;
+
+            var thingReq = new ThingRequest {singleDef = ThingDef.Named("ChickenNest")};
+            var chickenNest =
+                (Building_Bed)
+                GenClosest.ClosestThingReachable(pawn.Position, pawn.Map, thingReq, PathEndMode.OnCell,
+                    TraverseParms.For(pawn, Danger.None, TraverseMode.ByPawn, false), maxDistance: MaxSearchDistance);
+            return new Job(JobDefOf.LayEgg, (LocalTargetInfo)chickenNest.Position);
+        }
+    }
+}
