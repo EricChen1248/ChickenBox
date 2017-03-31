c14f256082afde15689ec59fbb7cc2bbc7bfaff9
EricChen1248 <ericchen1248@gmail.com>
2017-04-01 00:21:42 +0800

    Changed code for animal sleeping box

--- ChickenBox/_JobGiver_LayEgg.cs
+++ ChickenBox/_JobGiver_LayEgg.cs
@@ -17,15 +17,16 @@ namespace Chicken_Nest
             if (comp == null || !comp.CanLayNow)
                 return null;
 
-            var thingReq = new ThingRequest {singleDef = ThingDef.Named("ChickenNest")};
-            var chickenNest =
+            var thingReq = new ThingRequest { singleDef = ThingDef.Named("AnimalSleepingBox") };
+            var sleepingBox =
                 (Building_Bed)
                 GenClosest.ClosestThingReachable(pawn.Position, pawn.Map, thingReq, PathEndMode.OnCell,
                     TraverseParms.For(pawn, Danger.None, TraverseMode.ByPawn, false), maxDistance: MaxSearchDistance);
-            if (chickenNest != null)
+            if (sleepingBox != null)
             {
-                return new Job(JobDefOf.LayEgg, (LocalTargetInfo)chickenNest.Position);
+                return new Job(JobDefOf.LayEgg, (LocalTargetInfo)sleepingBox.Position);
             }
+            
             var intVec3 = RCellFinder.RandomWanderDestFor(pawn, pawn.Position, 5f,null, Danger.Some);
             return new Job(JobDefOf.LayEgg, (LocalTargetInfo)intVec3);
         }
